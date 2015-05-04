f7b64d741cefea54e9536503a5931573ee96a5fe
rlutter <regis.lutter@gmail.com>
2015-05-04 15:50:31 +0200

    Adapt fork for personal use

--- functions.php
+++ functions.php
@@ -40,7 +40,7 @@ function wpstarter_style_scripts() {
     wp_dequeue_style('wpforge');
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
-    wp_enqueue_script( 'wpstarter-js', get_stylesheet_directory_uri() . '/js/wpstarter-functions.js', array(), '', true );
+    wp_enqueue_script( 'child-js', get_stylesheet_directory_uri() . '/js/script.js', array(), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpstarter_style_scripts');
 ?>
